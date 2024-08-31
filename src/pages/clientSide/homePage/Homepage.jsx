4ade323a1344eef8453cf02eec20c2f117561fc1
ashiku2 <ashikur1602@gmail.com>
2024-08-31 23:33:04 +0600

    working with homepage

---
+++ src/pages/clientSide/homePage/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div>
+      <img src="https://res.cloudinary.com/dramj404v/image/upload/v1725125363/SkillStationIT/Homepage/q9se3xginiybxn8e6dln.png" alt="" />
+    </div>
+  );
+};
+
+export default Homepage;
